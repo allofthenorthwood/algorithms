387ea21daaaac8f5c5b15d303440b76e9a15c3c7
Ben Alpert <ben@benalpert.com>
2015-04-16 21:10:40 -0700

    Use babel-loader instead of jsx-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
             {
                 //tell webpack to use jsx-loader for all *.jsx files
                 test: /\.jsx$/,
-                loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+                loader: 'babel-loader'
             }
         ]
     },
